0d461778497a68413baef281520dd0316701f8c6
zhuzhu01 <zhuzhu01@baidu.com>
2018-04-23 10:27:24 +0800

    code style rev

--- src/mqtt_client.c
+++ src/mqtt_client.c
@@ -1255,7 +1255,7 @@ void mqtt_client_dowork(MQTT_CLIENT_HANDLE handle)
                         (void)sendPacketItem(mqtt_client, BUFFER_u_char(pingPacket), size);
                         BUFFER_delete(pingPacket);
                         if (mqtt_client->timeSincePing == 0) {
-                        (void)tickcounter_get_current_ms(mqtt_client->packetTickCntr, &mqtt_client->timeSincePing);
+                            (void)tickcounter_get_current_ms(mqtt_client->packetTickCntr, &mqtt_client->timeSincePing);
                         }
 
                         if (mqtt_client->logTrace)
